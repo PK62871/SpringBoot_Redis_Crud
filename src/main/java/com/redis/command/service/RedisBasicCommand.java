8e825f8f35be3c40ae4f8efb5ee335a3602c5dc1
Prabhakar Kumar <prabhakar62871@gmail.com>
2026-02-03 21:34:58 +0530

    Redis Basic Common Command

---
+++ src/main/java/com/redis/command/service/RedisBasicCommand.java
@@ -0,0 +1,62 @@
+package com.redis.command.service;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisBasicCommand {
+
+    private RedisTemplate<String,Object> redisTemplate;
+
+    public RedisBasicCommand(RedisTemplate<String,Object> redisTemplate){
+        this.redisTemplate = redisTemplate;
+    }
+
+   // @PostConstruct
+    public void trial(){
+
+        //Set Key Value
+        redisTemplate.opsForValue().set("trial","Prabhakar");
+        System.out.println("Inserted");
+
+        //rename Key
+       redisTemplate.rename("trial","name");
+
+        //set Expiry of key
+        Boolean expiry = redisTemplate.expire("name", 20, TimeUnit.SECONDS);
+        System.out.println("Is Expiry Set :- "+ expiry);
+
+        //Get Remaining  Time ........
+        Long ttl = redisTemplate.getExpire("name");
+        System.out.println("TTL :- "+ ttl);
+
+        //Get Remaining Time of key  in Second.....
+        //Here Value Will get override if key is already present
+        Long remaining = redisTemplate.getExpire("name", TimeUnit.SECONDS);
+        System.out.println("Remaining :- "+ remaining);
+
+        //Rename The Key If New Keu is not already Present So Value will not get Override
+        // In above renamed Value will get override
+        Boolean b = redisTemplate.renameIfAbsent("name", "name");
+        System.out.println("Renamed If Absent :- "+ b);
+
+
+        //Remove Expiry From Key..............
+        Boolean removedTtl = redisTemplate.persist("name");
+        System.out.println("TTL  Removed:- "+ removedTtl);
+
+        //Delete a Single Key or Multiple Key at a Same time...........
+        //But This is synchronous command..
+
+       // Boolean deleted = redisTemplate.delete("name");
+        //System.out.println("Deleted :- "+ deleted);
+
+        //Delete Key as single or multiple at a time asynchronously
+        Boolean name = redisTemplate.unlink("name");
+        System.out.println("Unlinked Name :- "+ name);
+
+    }
+}
---
+++ src/test/java/com/redis/command/RedisCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
